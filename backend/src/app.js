b9731063cd06909041a064de6d84df44619a17ce
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-07-23 11:29:12 -0600

    changes to path

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
