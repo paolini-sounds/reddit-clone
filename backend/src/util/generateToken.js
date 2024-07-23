14874c394d43e90a7e1b6b8c55f2abac03ab5c87
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-07-23 09:29:43 -0600

    changes to cookie settings

--- backend/src/util/generateToken.js
+++ backend/src/util/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie('jwt', token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000,
 		httpOnly: true, // prevent xss attacks
-		sameSite: 'strict', // CSRF attacks
+		sameSite: 'lax',
 		secure: process.env.NODE_ENV !== 'development',
 	});
 };
