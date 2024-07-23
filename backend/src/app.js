49494e3a42af6237ca8705b68746073555375bc3
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-07-23 11:32:09 -0600

    fixed static path

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,7 +36,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/subreddits', subredditRoutes);
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+	res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
 app.get('/', (req, res) => {
